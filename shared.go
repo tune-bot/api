6adc331f89780994fd5b24d3f36d81d347dde383
bronson-g <development@graansma.dev>
2023-07-19 20:26:33 -0400

    log errors

--- shared.go
+++ shared.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -15,6 +16,7 @@ func errorResponse(err error, w http.ResponseWriter) {
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest) // TODO use more descriptive http codes
+		fmt.Println(err.Error())
 		w.Write([]byte("{\"error\":\"" + strings.Replace(err.Error(), "\"", "\\\"", -1) + "\"}"))
 	}
 }
